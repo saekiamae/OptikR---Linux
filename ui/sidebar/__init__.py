717be98ecab87ec649260ef2f5fc8f40ea99a113
OptikR <243767949+OptikR@users.noreply.github.com>
2026-03-14 13:44:11 +0100

    Delete ui directory

--- ui/sidebar/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Sidebar Module
-
-Sidebar components for the Real-Time Translation System.
-"""
-
-from .sidebar_widget import SidebarWidget
-
-__all__ = ['SidebarWidget']
--- ui/toolbar/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Toolbar Module
-
-Toolbar components for the Real-Time Translation System.
-"""
-
-from .main_toolbar import MainToolbar
-
-__all__ = ['MainToolbar']
